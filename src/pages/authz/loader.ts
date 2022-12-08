2bb1b7abd853514dcf92fa071950f8b37d0271d7
André Sá de Mello <asmello@pm.me>
2022-12-08 10:58:06 +0000

    refactor: custom hook useStateUpdater

--- src/pages/authz/loader.ts
+++ src/pages/authz/loader.ts
@@ -1,5 +1,5 @@
 import { json } from "react-router-dom";
-import { AuthzConfigurationData } from "./AuthzConfiguration";
+import { AuthzConfigurationData, STATE_KEY } from "./AuthzConfiguration";
 
 function emptyConfiguration(): AuthzConfigurationData {
   return {
@@ -11,15 +11,13 @@ function emptyConfiguration(): AuthzConfigurationData {
   };
 }
 
-export const STORE_KEY = "authzState";
-
 export default async function (): Promise<Response> {
-  const persistentState = window.localStorage.getItem(STORE_KEY);
+  const persistentState = window.localStorage.getItem(STATE_KEY);
   if (persistentState) {
     return json<AuthzConfigurationData>(JSON.parse(persistentState));
   }
 
-  const ephemeralState = window.sessionStorage.getItem(STORE_KEY);
+  const ephemeralState = window.sessionStorage.getItem(STATE_KEY);
   if (ephemeralState) {
     return json<AuthzConfigurationData>(JSON.parse(ephemeralState));
   }
